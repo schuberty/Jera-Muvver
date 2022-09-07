d0e791c95bf40bdd84221f90d20b5bc67941dc9b
G. Schubert <schubertyi@gmail.com>
2022-09-07 13:24:31 -0300

    Implemented passenger cubit state and add package date model

---
+++ lib/src/modules/passenger/presentation/model/package_dates_model.dart
@@ -0,0 +1,9 @@
+class TransportDateModel {
+  final DateTime departureDate;
+  final DateTime arrivalDate;
+
+  TransportDateModel({
+    required this.departureDate,
+    required this.arrivalDate,
+  });
+}
